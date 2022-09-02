d6ede5fe21d2a5af5acf525091cff1e11425f75c
lance_MacBook_Pro <foleylance93@gmail.com>
2022-09-02 10:05:40 -0500

    add secure password for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
                     length: {minimum: 3, maximum: 25}
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 end
